eb9b0765da6f65be2c28b106adcd7979c0fe0688
Kavinya K <kavinyakarthi140@gmail.com>
2026-03-07 23:35:55 +0530

    Add Java solution to sort array of 0s, 1s, and 2s

---
+++ Day_02/03_Sort 0s, 1s and 2s.java
@@ -0,0 +1,11 @@
+// Problem: Second Largest Element
+// Platform: GeeksforGeeks
+// Language: Java
+// Difficulty: Easy
+// Link: https: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1?page=1&category=Arrays,Java&sortBy=submissions
+
+ class Solution {
+    public void sort012(int[] arr) {
+         Arrays.sort(arr);
+    }
+}
