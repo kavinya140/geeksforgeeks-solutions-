5c7cfadf0eeeb9d9c3e89f04dc077d5eaa32c021
Kavinya K <kavinyakarthi140@gmail.com>
2026-07-30 10:03:33 +0530

    Create 02_ValidPalindrome.java

---
+++ Day_11/02_ValidPalindrome.java
@@ -0,0 +1,31 @@
+//Problem: ValidPalindrome
+//Platform: Leetcode
+//Difficulty: Easy
+//Link: https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+   public boolean isPalindrome(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        // Skip non-alphanumeric characters on the left
+        while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+            left++;
+        }
+        // Skip non-alphanumeric characters on the right
+        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            right--;
+        }
+
+        // Compare characters (case-insensitive)
+        if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+    return true;
+}
+}
