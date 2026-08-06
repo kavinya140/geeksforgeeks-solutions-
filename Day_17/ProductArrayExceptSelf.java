f1bd200fedb4594b91196f55cb3960eecced8390
Kavinya K <kavinyakarthi140@gmail.com>
2026-08-06 09:34:26 +0530

    Create ProductArrayExceptSelf.java

---
+++ Day_17/ProductArrayExceptSelf.java
@@ -0,0 +1,19 @@
+//Platform: Neetcode
+//Link: https://neetcode.io/problems/products-of-array-discluding-self/history?list=blind75&submissionIndex=0
+//Difficulty: Medium
+//Language: Java
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]=new int[nums.length];
+         ans[0]=1;
+        for(int i=1;i<nums.length;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int right=1;
+        for(int i=nums.length-1;i>=0;i--){
+            ans[i]=ans[i]*right;
+            right=right*nums[i];
+        }
+        return ans;
+    }
+}  
