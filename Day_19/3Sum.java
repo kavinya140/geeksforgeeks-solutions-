1f6b468f4a6e75a9b6760b67d85c729e596d7a04
Kavinya K <kavinyakarthi140@gmail.com>
2026-08-08 09:46:20 +0530

    Refactor threeSum method for improved efficiency

--- Day_19/3Sum.java
+++ Day_19/3Sum.java
@@ -4,20 +4,33 @@
 //Link: https://neetcode.io/problems/three-integer-sum
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res=new HashSet<>();
-        int n=nums.length;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    int sum=nums[i]+nums[j]+nums[k];
-                    if(sum==0){
-                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k]);
-                        res.add(temp);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
                     }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-        return new ArrayList<>(res);
+        return res;
     }
 }
