d826c44771c40baf663d0410e66847cf29f73e70
Kavinya K <kavinyakarthi140@gmail.com>
2026-08-08 10:20:14 +0530

    Add Solution class for Container With Most Water problem
    Implement the maxArea method to calculate the maximum water container area.

---
+++ Day_19/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+//Language: Java
+//Difficulty: Medium
+//Platform: Neetcode
+//Link: https://neetcode.io/problems/max-water-container/history?list=neetcode150&submissionIndex=0
+class Solution {
+    public int maxArea(int[] heights) {
+        int left=0;
+        int right=heights.length-1;
+        int max=0;
+        while(left<right){
+            int width=right-left;
+            int h=Math.min(heights[left],heights[right]);
+            int area=width*h;
+            max=Math.max(max,area);
+            if(heights[left]< heights[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+                    }
+                    return max;
+    }
+}
